01bb3897a0ff0d291ad8e7e9b08a73965e96b599
Amar NRK <davidabhram47nrk@gmail.com>
2024-11-20 11:20:19 +0530

    last changed code final out with error

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+#include <Preferences.h>
 
 /***********************************************************************/
     #if defined(ESP32)
@@ -10,56 +10,73 @@
     #include <Firebase_ESP_Client.h>
     #include <addons/TokenHelper.h>
     #include <addons/RTDBHelper.h>
-    #define WIFI_SSID "Embedded"
-    #define WIFI_PASSWORD "Embedded54321"
-    #define API_KEY "AIzaSyBUwmFxX4j66PN6sKQwSIScxEcdtEj2M98"
-    #define DATABASE_URL "esp8266-8abda-default-rtdb.firebaseio.com"
-    #define USER_EMAIL "chamchim546@gmail.com"
-    #define USER_PASSWORD "chimcham12345"
+    #define WIFI_SSID "S 20 FE"
+    #define WIFI_PASSWORD "123456710"
+    #define API_KEY "AIzaSyDt2Q1wUwFogJFAPLstSVgF9xT5UijgKYY"
+    #define DATABASE_URL "https://wifi-crd-change-default-rtdb.firebaseio.com"
+    #define USER_EMAIL "davidabhram47nrk@gmail.com"
+    #define USER_PASSWORD "wifi12345"
+
     FirebaseData fbdo;
     FirebaseAuth auth;
     FirebaseConfig config;
+    Preferences preferences;
 /***********************************************************************/
     int count = 0 ;
     String msg;
     int num = 0;
+    String WIFI_SSID_1;
+    String WIFI_PASSWORD_1;
+    int F = 0;
 /***********************************************************************/
+    int Extract_integer_from_string(String x);
     void Connect_WiFi();
     void Firebase_Store(String PATH,String MSG);
     String Firebase_getString(String PATH);
+    void Connect_WiFi_1();
+    void write_Credentials(const String& ssid, const String& password);
+    void read_Credentials(String &ssid, String &password);
+    void read_F(int &F);
+    void write_F(int F);
 /***********************************************************************/
     void setup()
-    {
-         Connect_WiFi(); 
+    { 
+      preferences.begin("wifiCreds", true);
+      WiFi.disconnect();
     }
 /***********************************************************************/
     void loop()
     {
-        if(Serial.available())
-        {
-            char x;
-            x=Serial.read();
-            if(x=='A')
+      int key;
+      read_F(key);
+      String CONNECT;
+      while (WiFi.status() != WL_CONNECTED)
+      {
+          if (key == 1)
             {
-              Firebase_Store("/LED/DATA","ON");
-              digitalWrite(2,LOW);             
+              Connect_WiFi_1();
             }
-             else if(x=='B')
+            else
             {
-               Firebase_Store("/LED/DATA","OFF");
-               digitalWrite(2,HIGH); 
+              Connect_WiFi();
             }
-            else if(x=='C')
-            {
-              msg = Firebase_getString("/LED/DATA");
-              Serial.println(msg);
-            }
-        }  
-                     
+      }
+       
+      preferences.begin("wifiCreds", false);
+      WIFI_SSID_1 = Firebase_getString("/SSID");
+      WIFI_PASSWORD_1 = Firebase_getString("/PASSWORD");
+      write_Credentials(WIFI_SSID_1, WIFI_PASSWORD_1);
+      CONNECT = Firebase_getString("/CONNECT");
+      if(CONNECT == "ON")
+      {
+        esp_restart();
+      }
     }
 /***********************************************************************/
     void Connect_WiFi()
     {
+      F = 1;
+      write_F(F);
           Serial.begin(9600);
           delay(100); 
           WiFi.disconnect();
@@ -90,6 +107,7 @@
           Firebase.reconnectWiFi(true);
           Firebase.setDoubleDigits(5);
           config.timeout.serverResponse = 10 * 1000;
+          Firebase_Store("/STATUS","CONNECTED");
     }
 /***********************************************************************/
     void Firebase_Store(String PATH,String MSG)
@@ -110,6 +128,75 @@
       return msg;
     }
 /***********************************************************************/
-    
+  int Extract_integer_from_string(String x)
+{
+    int num=0,i;
+    for(i=0;x[i]!='\0';i++)
+    {
+        num = num * 10 + (x[i] - 48);
+    }
+    return num;
+   
+}  
 /***********************************************************************/
+void write_Credentials(const String& ssid, const String& password) 
+{
+    preferences.putString("ssid", ssid);
+    preferences.putString("password", password);
+}
+/***********************************************************************/
+void read_Credentials(String &ssid, String &password) 
+{
+    ssid = preferences.getString("ssid", "defaultSSID");       // "defaultSSID" if not found
+    password = preferences.getString("password", "defaultPW"); // "defaultPW" if not found
+}
+/***********************************************************************/
+void write_F(int F) 
+{
+    preferences.putInt("F", F);              // Store the integer F
+}
+/***********************************************************************/
+void read_F(int &F) 
+{
+    F = preferences.getInt("F", 0);  // Default value 0 if not found
+}
+/***********************************************************************/
+void Connect_WiFi_1()
+{
+
+  read_Credentials(WIFI_SSID_1, WIFI_PASSWORD_1);
+          Serial.begin(9600);
+          delay(100); 
+          WiFi.disconnect();
+          delay(800); 
+          Serial.println("Connecting to Wi-Fi"); 
+          WiFi.begin(WIFI_SSID_1, WIFI_PASSWORD_1);
+          Serial.print("Connecting to Wi-Fi_1");
+          delay(100);
+          while (WiFi.status() != WL_CONNECTED)
+          {
+            Serial.print(".");
+            delay(300);
+          }
+          Serial.println();
+          Serial.print("Connected with IP: ");
+          Serial.println(WiFi.localIP());
+          Serial.println();
+          Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+          config.api_key = API_KEY;
+          auth.user.email = USER_EMAIL;
+          auth.user.password = USER_PASSWORD;
+          config.database_url = DATABASE_URL;
+          config.token_status_callback = tokenStatusCallback;
+          #if defined(ESP8266)
+            fbdo.setBSSLBufferSize(2048 /* Rx buffer size in bytes from 512 - 16384 */, 2048 /* Tx buffer size in bytes from 512 - 16384 */);
+          #endif
+          Firebase.begin(&config, &auth);
+          Firebase.reconnectWiFi(true);
+          Firebase.setDoubleDigits(5);
+          config.timeout.serverResponse = 10 * 1000;
+          Firebase_Store("/STATUS","CONNECTED_1");
+  preferences.begin("wifiCreds", false);
+  write_Credentials(WIFI_SSID_1, WIFI_PASSWORD_1);
+}
 /***********************************************************************/
